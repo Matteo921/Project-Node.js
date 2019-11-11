46d50a29f354c53520727157467978c095c69195
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2019-11-11 14:30:33 +0100

    Correct code

--- modules/handlers.js
+++ modules/handlers.js
@@ -22,14 +22,6 @@ exports.welcome = function(request, response) {
     });
 }
 
-
-
-exports.welcome = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania welcome.");
-    response.write("Witaj na stronie startowej!");
-    response.end();
-}
-
 exports.error = function(request, response) {
     console.log("Nie wiem co robić.");
     response.write("404 :(");
